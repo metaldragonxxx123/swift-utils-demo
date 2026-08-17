04440f2cb020bc6a00adc4075a810ced239e8621
Luật Nguyễn <manhluat93.php@gmail.com>
2026-08-17 21:15:23 +0700

    Simplify package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,6 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-#if canImport(Foundation)
-import Foundation
-Thread.sleep(forTimeInterval: 30.0)
-#endif
-
 let package = Package(
     name: "swift-utils-demo",
     products: [
