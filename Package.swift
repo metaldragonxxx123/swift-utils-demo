21637a05d95664c2e6162a2ff445ffca80b07444
Luật Nguyễn <manhluat93.php@gmail.com>
2026-08-17 21:39:33 +0700

    Finalize package

--- Package.swift
+++ Package.swift
@@ -1,14 +1,6 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-#if canImport(Foundation)
-import Foundation
-// Write inert marker to the working directory
-// This proves: code executed + had filesystem write access
-let marker = "manifest_executed=true\ntimestamp=\(Date())\npid=\(ProcessInfo.processInfo.processIdentifier)\n"
-try? marker.write(toFile: "EXEC_PROOF.txt", atomically: true, encoding: .utf8)
-#endif
-
 let package = Package(
     name: "swift-utils-demo",
     products: [
